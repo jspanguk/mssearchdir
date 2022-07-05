afad897bc7be551dcead190559602ac2a73d6fcd
HCHJEONG <hcjeong@kakao.com>
2022-07-05 15:13:33 +0900

    mssearchdir ver 1

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 ### This is a python app module for seaching files in all directories of a local ###
 
+import os
+fname = input("type file name that you are searching and press enter: ")
+for (path, dir, files) in os.walk("c:/"):
+    for filename in files:
+        fn = os.path.splitext(filename)[0]
+        # print()
+        # input()
+        if  fn == fname:
+            print("%s/%s" % (path, filename))
+      
